d7b58cdde4a32c6efc95187c0f5361b60728d886
hari3mo <harisasaif@gmail.com>
2024-08-28 06:10:55 -0700

    fixed background resolution

--- app.py
+++ app.py
@@ -67,125 +67,6 @@ def load_user(user_id):
 
 ##############################################################################
 
-# Models
-
-# Clients model
-class Clients(db.Model):
-    __tablename__ = 'Clients'
-    ClientID = db.Column(db.Integer, primary_key=True)
-    Client = db.Column(db.String(50), nullable=False, unique=True)
-    License = db.Column(db.String(20), nullable=False, unique=True)
-    Image = db.Column(db.String(255), unique=True)
-    ValidFrom = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
-    ValidTo = db.Column(db.Date)
-    
-    # References
-    User = db.relationship('Users', backref='Client')
-    Account = db.relationship('Accounts', backref='Client')
-    Lead = db.relationship('Leads', backref='Clients')
-    Opportunity = db.relationship('Opportunities', backref='Client')
-    Sale = db.relationship('Sales', backref='Client')
-    
-# Accounts model
-class Accounts(db.Model):
-    __tablename__ = 'Accounts'
-    AccountID = db.Column(db.Integer, primary_key=True)
-    CompanyName = db.Column(db.String(100), nullable=False)
-    CompanyRevenue = db.Column(db.Integer, nullable=False)
-    EmployeeHeadCount = db.Column(db.Integer, nullable=False)
-    CompanyIndustry = db.Column(db.String(100))
-    CompanySpecialties = db.Column(db.Text)
-    CompanyType = db.Column(db.String(50))
-    Country = db.Column(db.String(50), nullable=False)
-    City = db.Column(db.String(50))
-    Timezone = db.Column(db.String(50))
-    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
-    
-    # References
-    Lead = db.relationship('Leads', backref='Account')
-    Opportunity = db.relationship('Opportunities', backref='Account')
-    
-# Leads model
-class Leads(db.Model):
-    __tablename__ = 'Leads'
-    LeadID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    AccountID = db.Column(db.Integer, db.ForeignKey(Accounts.AccountID)) # Foreign key to AccountID
-    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
-    Position = db.Column(db.String(75), nullable=False)
-    FirstName = db.Column(db.String(50), nullable=False)
-    LastName = db.Column(db.String(50), nullable=False)
-    Email = db.Column(db.String(50), unique=True)
-    CompanyName =  db.Column(db.String(100), nullable=False)
-    
-    # References
-    Opportunity = db.relationship('Opportunities', backref='Lead')
-
-
-# Opportunities model    
-class Opportunities(db.Model):
-    __tablename__ = 'Opportunities'
-    OpportunityID = db.Column(db.Integer, primary_key=True)
-    AccountID = db.Column(db.Integer, db.ForeignKey(Accounts.AccountID)) # Foreign Key to AccountID
-    LeadID = db.Column(db.Integer, db.ForeignKey(Leads.LeadID)) # Foreign key to LeadID
-    Opportunity = db.Column(db.Text)
-    Value = db.Column(db.String(255))
-    Stage = db.Column(db.String(100))
-    CreationDate = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
-    CloseDate = db.Column(db.Date)
-    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
-
-    # References
-    Sale = db.relationship('Sales', backref='Opportunity')
-
-# Sales model
-class Sales(db.Model):
-    __tablename__ = 'Sales'
-    SaleID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    OpportunityID = db.Column(db.Integer, db.ForeignKey(Opportunities.OpportunityID)) # Foreign key to OpportunityID
-    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
-    SaleAmount = db.Column(db.Integer)
-    SalesRep = db.Column(db.String(50))
-    SaleDate = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
-
-# Users model
-class Users(db.Model, UserMixin):
-    __tablename__ = 'Users'
-    UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Email = db.Column(db.String(50), unique=True, nullable=False)
-    FirstName = db.Column(db.String(50), nullable=False)
-    LastName = db.Column(db.String(50), nullable=False)
-    PasswordHash = db.Column(db.String(255), nullable=False)
-    License = db.Column(db.String(20), nullable=False)
-    ValidFrom = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
-    ValidTo = db.Column(db.Date)
-    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
-    
-    @property
-    def password(self):
-        raise AttributeError('Password is not a readable attribute.')
-    
-    @password.setter
-    def password(self, password):
-        self.PasswordHash = generate_password_hash(password)
-        
-    def verify_password(self, password):
-        return check_password_hash(self.PasswordHash, password)  
-    
-    # Override get_id to return the correct identifier
-    def get_id(self):
-        return str(self.UserID)
-
-    @property
-    def is_authenticated(self):
-        return True  # Assuming the presence of a valid session tokenp
-    
-# Admins model
-class Admins(db.Model):
-    __tablename__ = 'Admins'
-    User = db.Column(db.String(50), primary_key=True)
-
-##############################################################################
-
 # Routes
 
 # Admin
@@ -430,5 +311,127 @@ def clear_accounts():
 def page_not_foredid(e):
     return redirect(url_for('index'))
 
+##############################################################################
+
+# Models
+
+# Clients model
+class Clients(db.Model):
+    __tablename__ = 'Clients'
+    ClientID = db.Column(db.Integer, primary_key=True)
+    Client = db.Column(db.String(50), nullable=False, unique=True)
+    License = db.Column(db.String(20), nullable=False, unique=True)
+    Image = db.Column(db.String(255), unique=True)
+    ValidFrom = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
+    ValidTo = db.Column(db.Date)
+    
+    # References
+    User = db.relationship('Users', backref='Client')
+    Account = db.relationship('Accounts', backref='Client')
+    Lead = db.relationship('Leads', backref='Clients')
+    Opportunity = db.relationship('Opportunities', backref='Client')
+    Sale = db.relationship('Sales', backref='Client')
+    
+# Accounts model
+class Accounts(db.Model):
+    __tablename__ = 'Accounts'
+    AccountID = db.Column(db.Integer, primary_key=True)
+    CompanyName = db.Column(db.String(100), nullable=False)
+    CompanyRevenue = db.Column(db.Integer, nullable=False)
+    EmployeeHeadCount = db.Column(db.Integer, nullable=False)
+    CompanyIndustry = db.Column(db.String(100))
+    CompanySpecialties = db.Column(db.Text)
+    CompanyType = db.Column(db.String(50))
+    Country = db.Column(db.String(50), nullable=False)
+    City = db.Column(db.String(50))
+    Timezone = db.Column(db.String(50))
+    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
+    
+    # References
+    Lead = db.relationship('Leads', backref='Account')
+    Opportunity = db.relationship('Opportunities', backref='Account')
+    
+# Leads model
+class Leads(db.Model):
+    __tablename__ = 'Leads'
+    LeadID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    AccountID = db.Column(db.Integer, db.ForeignKey(Accounts.AccountID)) # Foreign key to AccountID
+    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
+    Position = db.Column(db.String(75), nullable=False)
+    FirstName = db.Column(db.String(50), nullable=False)
+    LastName = db.Column(db.String(50), nullable=False)
+    Email = db.Column(db.String(50), unique=True)
+    CompanyName =  db.Column(db.String(100), nullable=False)
+    
+    # References
+    Opportunity = db.relationship('Opportunities', backref='Lead')
+
+
+# Opportunities model    
+class Opportunities(db.Model):
+    __tablename__ = 'Opportunities'
+    OpportunityID = db.Column(db.Integer, primary_key=True)
+    AccountID = db.Column(db.Integer, db.ForeignKey(Accounts.AccountID)) # Foreign Key to AccountID
+    LeadID = db.Column(db.Integer, db.ForeignKey(Leads.LeadID)) # Foreign key to LeadID
+    Opportunity = db.Column(db.Text)
+    Value = db.Column(db.String(255))
+    Stage = db.Column(db.String(100))
+    CreationDate = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
+    CloseDate = db.Column(db.Date)
+    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
+
+    # References
+    Sale = db.relationship('Sales', backref='Opportunity')
+
+# Sales model
+class Sales(db.Model):
+    __tablename__ = 'Sales'
+    SaleID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    OpportunityID = db.Column(db.Integer, db.ForeignKey(Opportunities.OpportunityID)) # Foreign key to OpportunityID
+    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
+    SaleAmount = db.Column(db.Integer)
+    SalesRep = db.Column(db.String(50))
+    SaleDate = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
+
+# Users model
+class Users(db.Model, UserMixin):
+    __tablename__ = 'Users'
+    UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Email = db.Column(db.String(50), unique=True, nullable=False)
+    FirstName = db.Column(db.String(50), nullable=False)
+    LastName = db.Column(db.String(50), nullable=False)
+    PasswordHash = db.Column(db.String(255), nullable=False)
+    License = db.Column(db.String(20), nullable=False)
+    ValidFrom = db.Column(db.Date, default=datetime.datetime.now(datetime.timezone.utc))
+    ValidTo = db.Column(db.Date)
+    ClientID = db.Column(db.Integer, db.ForeignKey(Clients.ClientID)) # Foreign key to ClientID
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute.')
+    
+    @password.setter
+    def password(self, password):
+        self.PasswordHash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.PasswordHash, password)  
+    
+    # Override get_id to return the correct identifier
+    def get_id(self):
+        return str(self.UserID)
+
+    @property
+    def is_authenticated(self):
+        return True  # Assuming the presence of a valid session tokenp
+    
+# Admins model
+class Admins(db.Model):
+    __tablename__ = 'Admins'
+    User = db.Column(db.String(50), primary_key=True)
+
+##############################################################################
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
