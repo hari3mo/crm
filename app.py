5833d34671d145e89d9a6068f63a5d4b0291d0d5
hari3mo <harisasaif@gmail.com>
2024-09-16 18:24:35 -0700

    removed leads list limit

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ def leads_list():
         else:
             leads = leads.filter_by(FollowUp=False)
     
-    return render_template('leads/leads_list.html', leads=leads.limit(20), companies=companies,
+    return render_template('leads/leads_list.html', leads=leads.all(), companies=companies,
         positions=positions, cities=cities, owners=owners)
 
 
